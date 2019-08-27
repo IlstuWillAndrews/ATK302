a27c9aeab7d594b957ac986e75ead7ba803f3026
IlstuWillAndrews <wrandr1@ilstu.edu>
2019-08-27 11:48:51 -0500

    Trees

--- p5/tree/sketch.js
+++ p5/tree/sketch.js
@@ -1,28 +1,66 @@
 function setup() {
 
   createCanvas(720, 400);
-    background(0);
-    noStroke();
 
-    fill(204);
-    triangle(18, 18, 18, 360, 81, 360);
-
-    fill(102);
-    rect(81, 81, 63, 63);
-
-    fill(204);
-    quad(189, 18, 216, 18, 216, 360, 144, 360);
-
-    fill(255);
-    ellipse(252, 144, 72, 72);
-
-    fill(204);
-    triangle(288, 18, 351, 360, 288, 360);
-
-    fill(255);
-    arc(479, 300, 280, 280, PI, TWO_PI);
 }
 
 function draw() {
-  // put drawing code here
+  background("#6669ff");
+  noStroke();
+
+  fill("#7a6451");
+    rect(550, 150, 50, 350);
+
+  fill("#1eb02d");
+    triangle(575, 18, 675, 280, 475, 280);
+
+  fill("#d7ff26");
+    ellipse(10, 10, 200, 200);
+
+  fill(255);
+    arc(250, 90, 100, 150, PI, TWO_PI);
+
+    fill(255);
+      arc(350, 90, 100, 150, PI, TWO_PI);
+
+      fill(255);
+        arc(450, 90, 100, 150, PI, TWO_PI);
+
+        fill(255);
+          rect(200, 80, 300, 50);
+
+          fill("#42f56c");
+            rect(250, 325, 5, 75);
+
+            fill("#d6f700");
+              ellipse(252.5, 315, 35, 35);
+
+              fill(0);
+                ellipse(252.5, 315, 15, 15);
+
+                fill("#42f56c");
+                  rect(198.5, 325, 5, 75);
+
+                fill("#d6f700");
+                  ellipse(200, 315, 35, 35);
+
+                  fill(0);
+                    ellipse(200, 315, 15, 15);
+
+                    fill("#42f56c");
+                      rect(144.5, 325, 5, 75);
+
+                    fill("#d6f700");
+                      ellipse(147.5, 315, 35, 35);
+
+                      fill(0);
+                        ellipse(147.5, 315, 15, 15);
+
+
+
+          fill("#25c225");
+    rect(0, 375, 720, 50);
+
+fill(255)
+
 }
