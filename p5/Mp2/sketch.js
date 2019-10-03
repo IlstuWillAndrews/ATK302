d95623454000f431d4e9d5757785b8b500d9d2e7
IlstuWillAndrews <wrandr1@ilstu.edu>
2019-10-03 11:29:15 -0500

    good shit

--- p5/Mp2/sketch.js
+++ p5/Mp2/sketch.js
@@ -29,7 +29,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1500, 800);
   background("black");
   timer = 0;
   sun = loadImage("assets/god-like-sphere.png");
@@ -55,23 +55,24 @@ function state() {
 
     case 0:
       background("black")
-      birdCreate.position(width / 2 - 100, height / 2 + 100);
+      birdCreate.position(650, 500);
       birdCreate.size(200, 200)
       fill("white")
       textFont("Cora")
       textSize(80)
-      text("Sustinance for a Mindset", width / 4, height / 2);
+      smooth();
+      text("Sustenance for a Mindset", 375, 400);
       if (birdPlay == 0) {
         birdSounds.loop();
         birdPlay = 1;
       }
       fill("blue");
-      rect(width / 2 + 500, height / 2 + 250, 200, 100);
+      rect(1250, 650, 200, 100);
       fill("white");
-      rect(width / 2 + 512.5, height / 2 + 265, 175, 75);
+      rect(1262.5, 665, 175, 75);
       fill("black")
       textSize(35)
-      text("Start", width / 2 + 565, (height / 2) + 312.5)
+      text("Start", 1315, 712.5)
 
 
 
@@ -94,14 +95,16 @@ function state() {
         imageSize++
       }
 
-      image(sun, width / 2, height / 2, x, y)
+      image(sun, 750, 400, x, y)
       fill("white")
-      stroke("blue")
+
+      stroke("black")
       textFont("Cora")
       textSize(40)
+      smooth();
       if (timer < 1800) {
-        text(' If life is not what it seems and is but a dream \n of a God from a far away land', width / 4, height / 2 - 100)
-
+        text(' If life is not what it seems and is but a dream \n of a God from a faraway land', 375, 300)
+        smooth();
         if (playSong == 0) {
           chords.loop();
           playSong = 1;
@@ -113,7 +116,8 @@ function state() {
         timer++
       }
       if (timer > 1800) {
-        text(' If life is not what it seems and is but a dream \n of a God from a far away land \n undergoing eternity, navigating uncertainty \n Then if I think then therefore I am?', width / 4, height / 2 - 100)
+        text(' If life is not what it seems and is but a dream \n of a God from a faraway land \n undergoing eternity, navigating uncertainty \n Then if I think then therefore I am?', 375, 300)
+        smooth();
         chords.stop();
         if (playSong == 0) {
           melody.loop();
@@ -124,12 +128,12 @@ function state() {
 
 
       fill("blue");
-      rect(width / 2 + 500, height / 2 + 250, 200, 100);
+      rect(1250, 650, 200, 100);
       fill("white");
-      rect(width / 2 + 512.5, height / 2 + 265, 175, 75);
+      rect(1262.5, 665, 175, 75);
       fill("black")
       textSize(35)
-      text("Continue", width / 2 + 537.5, (height / 2) + 312.5)
+      text("Continue", 1287.5, 712.5)
 
 
       break;
@@ -142,14 +146,14 @@ function state() {
       chords.stop()
       melody.stop()
       background("black")
-      image(sun, width / 2, height / 2, x, y)
+      image(sun, 750, 400, x, y)
       fill("white")
-      stroke("blue")
+      stroke("black")
       textFont("Cora")
       textSize(40)
       if (timer < 1800) {
-        text(' For if I live boundless and brave; Sound and safe \n Am I still trapped in the winds of my fate', width / 4, height / 2 - 100)
-
+        text(' For if I live boundless and brave; Sound and safe \n Am I still trapped in the winds of my fate', 375, 300)
+        smooth();
         if (playSong == 0) {
           drums.loop();
           playSong = 1;
@@ -161,7 +165,8 @@ function state() {
         timer++
       }
       if (timer > 1800) {
-        text(' For if I live boundless and brave; Sound and safe \n Am I still trapped in the winds of my fate \n Am I a man living free, or an actor of plans \n living my time on a stage ', width / 4, height / 2 - 100)
+        text(' For if I live boundless and brave; Sound and safe \n Am I still trapped in the winds of my fate \n Am I a man living free, or an actor of plans \n living my time on a stage ', 375, 300)
+        smooth();
         drums.stop();
         if (playSong == 0) {
           bass.loop();
@@ -172,12 +177,12 @@ function state() {
 
 
       fill("blue");
-      rect(width / 2 + 500, height / 2 + 250, 200, 100);
+      rect(1250, 650, 200, 100);
       fill("white");
-      rect(width / 2 + 512.5, height / 2 + 265, 175, 75);
+      rect(1262.5, 665, 175, 75);
       fill("black")
       textSize(35)
-      text("Continue", width / 2 + 537.5, (height / 2) + 312.5)
+      text("Continue", 1287.5, 712.5)
 
       break;
     case 3:
@@ -190,14 +195,14 @@ function state() {
       }
 
       background("black")
-      image(sun, width / 2, height / 2, x, y)
+      image(sun, 750, 400, x, y)
       fill("white")
-      stroke("blue")
+      stroke("black")
       textFont("Cora")
       textSize(40)
       if (timer < 1800) {
-        text('"For I must be free" stirs within \n But sometimes I feel the strings pull', width / 4, height / 2 - 100)
-
+        text('"For I must be free" stirs within \n But sometimes I feel the strings pull', 375, 300)
+        smooth();
         if (playSong == 0) {
           pad.loop();
           playSong = 1;
@@ -209,7 +214,8 @@ function state() {
         timer++
       }
       if (timer > 1800) {
-        text('"For I must be free" stirs within \n But sometimes I feel the strings pull \n I am a puppet perhaps, doing a dance \n I created all on my own', width / 4, height / 2 - 100)
+        text('"For I must be free" stirs within \n But sometimes I feel the strings pull \n I am a puppet perhaps, doing a dance \n I created all on my own', 375, 300)
+        smooth();
         pad.stop();
         if (playSong == 0) {
           lead.loop();
@@ -220,12 +226,13 @@ function state() {
 
 
       fill("blue");
-      rect(width / 2 + 500, height / 2 + 250, 200, 100);
+      rect(1250, 650, 200, 100);
       fill("white");
-      rect(width / 2 + 512.5, height / 2 + 265, 175, 75);
+      rect(1262.5, 665, 175, 75);
       fill("black")
       textSize(35)
-      text("Continue", width / 2 + 537.5, (height / 2) + 312.5)
+      text("Continue", 1287.5, 712.5)
+
       break;
     case 4:
       lead.stop()
@@ -238,14 +245,13 @@ function state() {
       }
 
       background("black")
-      image(sun, width / 2, height / 2, x, y)
-      fill("white")
-      stroke("blue")
+      image(sun, 750, 400)
+      stroke("black")
       textFont("Cora")
       textSize(40)
       if (timer < 1780) {
-        text(' I must believe I am writing the script \n Each choice another stroke of the pen ', width / 4, height / 2 - 100)
-
+        text(' I must believe I am writing the script \n Each choice another stroke of the pen ', 375, 300)
+        smooth();
         if (playSong == 0) {
           leadmelody.loop();
           playSong = 1;
@@ -257,7 +263,8 @@ function state() {
         timer++
       }
       if (timer > 1780) {
-        text(' I must believe I am writing the script \n Each choice another stroke of the pen \n Who I am? I may never know \n But control is mine my friend', width / 4, height / 2 - 100)
+        text(' I must believe I am writing the script \n Each choice another stroke of the pen \n Who I am? I may never know \n But control is mine my friend', 375, 300)
+        smooth();
         leadmelody.stop();
         if (playSong == 0) {
           final.loop();
@@ -268,29 +275,51 @@ function state() {
 
 
       fill("blue");
-      rect(width / 2 + 500, height / 2 + 250, 200, 100);
+      rect(1250, 650, 200, 100);
       fill("white");
-      rect(width / 2 + 512.5, height / 2 + 265, 175, 75);
+      rect(1262.5, 665, 175, 75);
       fill("black")
       textSize(35)
-      text("Continue", width / 2 + 537.5, (height / 2) + 312.5)
+      text("Continue", 1287.5, 712.5)
       break;
+
+    case 5:
+      background("black")
+      birdCreate.position(650, 500);
+      birdCreate.size(200, 200)
+      fill("white")
+      textFont("Cora")
+      textSize(80)
+      smooth();
+      text("Sustenance for a Mindset", 375, 400);
+      if (birdPlay == 0) {
+        birdSounds.loop();
+        birdPlay = 1;
+      }
+      fill("blue");
+      rect(1250, 650, 200, 100);
+      fill("white");
+      rect(1262.5, 665, 175, 75);
+      fill("black")
+      textSize(35)
+      text("Restart", 1315, 712.5)
+
+
   }
 
 }
 
 function mouseReleased() {
-  if (myState < 4) {
-      if ((mouseX > width / 2 + 500) && (mouseX < width / 2 + 700) && (mouseY < height / 2 + 350) && (mouseY > height / 2 + 250))
-       {
-        myState++;
-        timer = 0;
-        imageSize = 0;
-        playSong = 0;
-        }
+  if (myState < 5) {
+    if ((mouseX > 1250) && (mouseX < 1450) && (mouseY < 750) && (mouseY > 650)) {
+      myState++;
+      timer = 0;
+      imageSize = 0;
+      playSong = 0;
+      birdPlay = 0;
     }
-    else if (myState >= 4) {
-      myState = 0;
+  } else if (myState >= 5) {
+    myState = 1;
   }
 
 }
